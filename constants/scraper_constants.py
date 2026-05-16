588bea9843e8932d1594d8fcfb88714a78a2266b
unknown <khalish.21muhammad07@gmail.com>
2026-05-16 19:56:48 +0700

    Adding scraper constants.

---
+++ constants/scraper_constants.py
@@ -0,0 +1,20 @@
+BASE_URL = "https://www.vlr.gg"
+
+HEADERS = {
+    "User-Agent": ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/142.0")
+}
+
+STAGE_KEYWORD = [
+    "masters",
+    "champions",
+    "stage 1",
+    "stage 2",
+    "kickoff"
+]
+
+REGION_KEYWORD = [
+    "pasific",
+    "emea",
+    "americas",
+    "china"
+]
