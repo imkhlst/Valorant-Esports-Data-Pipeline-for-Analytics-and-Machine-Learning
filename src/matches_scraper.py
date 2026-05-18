8a3b0d90c958273b59394ed6b9fc14dc1e7e696e
unknown <khalish.21muhammad07@gmail.com>
2026-05-18 21:47:26 +0700

    Optimizing and fixing minor bug.

--- src/matches_scraper.py
+++ src/matches_scraper.py
@@ -1,21 +1,20 @@
 from datetime import datetime
-from dataclasses import asdict
-from constants.scraper_contants import *
+from constants.scraper_constants import *
 from utils.scraper_utils import *
-from entities.match_entities import Match
+from entities.match_entities import *
 from logger import logging
 
 class MatchesScraper:
     def __init__(self):
         pass
 
-    def scrape_matches_list(self, page: list) -> list:
+    def scrape_matches_list(self, match_page: list) -> list:
+        start_time = datetime.now()
         processed = set()
-        queue = list(page) if isinstance(page, (list, set)) else [page]
+        queue = list(match_page) if isinstance(match_page, (list, set)) else [match_page]
         print(f"Queue: {queue[:3]}")
         try:
-            results = []
-            start_time = datetime.now()
+            matches_list = []
             for item in queue:
                 tour_id, url = item[0], item[1]
                 if url in processed:
@@ -23,30 +22,30 @@ class MatchesScraper:
                     continue
 
                 soup = get_soup(url=url)
-                match_containers = soup.select(".wf-card a[href]")
-                for match in match_containers:
-                    href = match.get("href")
-                    url = absolute(url=href)
-                    results.add([tour_id, url])
+                match_href = get_value(soup=soup, selector=".wf-module-item.match-item", attr="href", multiple=True)
+                for href in match_href:
+                    match_url = absolute(url=href)
+                    matches_list.append((tour_id, match_url))
                 
                 processed.add(url)
             
             end_time = datetime.now()
             duration = end_time - start_time
             logging.info(f"scrape_matches_list completed in {duration}s")
-            return results
+            return matches_list
 
         except Exception as e:
             logging.error(f"Error occurs when running scrape_matches_list: {e}")
             print(f"Error occurs when running scrape_matches_list: {e}")
     
-    def scrape_matches_info(self, page: list):
+    def scrape_matches_info(self, match_list: list):
+        start_time = datetime.now()
         processed = set()
-        queue = list(page) if isinstance(page, (list, set)) else [page]
+        queue = list(match_list) if isinstance(match_list, (set, list)) else [match_list]
         print(f"Queue: {queue[:3]}")
         try:
-            results = []
-            start_time = datetime.now()
+            matches_info = []
+            tab_list = []
             for item in queue:
                 tour_id, url = item[0], item[1]
                 if url in processed:
@@ -54,30 +53,36 @@ class MatchesScraper:
                     continue
 
                 soup = get_soup(url=url)
-                bracket = get_value(soup=soup, selector=".match-header-event-series", select_option=True)
-                match_id = get_value(soup=soup, selector=".vm-stats-tabnav a", value="data-match-id", select_option=True)
-                date = get_value(soup=soup, selector=".moment-tz-convert", value="data-utc-ts", select_option=True)
-                patch_info = get_value(soup=soup, selector=".match-header-date", select_option=True)
-                patch = patch_info.split("Patch")[-1].strip()
+                bracket = get_value(soup=soup, selector=".match-header-event-series", attr="text")
+                match_id = get_value(soup=soup, selector=".vm-stats-tabnav a", attr="data-match-id")
+                tab_elements = get_value(soup=soup, selector=".vm-stats-tabnav a", attr="href")
+                tab_list.append([match_id, sorted[tab_elements]])
+                date = get_value(soup=soup, selector=".moment-tz-convert", attr="data-utc-ts")
+                date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
+                patch_info = get_value(soup=soup, selector=".match-header-date", attr="text")
+                if "patch" in patch_info.lower():
+                    patch = patch_info.split("Patch")[-1].strip()
+                else:
+                    patch = None
                 
-                home_href = get_value(soup=soup, selector=".match-header-link.wf-link-hover.mod-1", select_option=True)
+                home_href = get_value(soup=soup, selector=".match-header-link.wf-link-hover.mod-1", attr="href")
                 home_url = absolute(url=home_href)
-                home_soup = get_soup(home_url)
-                home_info = get_value(soup=home_soup, selector=".wf-title")
+                home_soup = get_soup(url=home_url)
+                home_info = get_value(soup=home_soup, selector=".wf-title", attr="text", multiple=True)
                 home_name, home_alias = home_info[0], home_info[1]
 
-                away_href = get_value(soup=soup, selector=".match-header-link.wf-link-hover.mod-2", select_option=True)
+                away_href = get_value(soup=soup, selector=".match-header-link.wf-link-hover.mod-2", attr="href")
                 away_url = absolute(url=away_href)
-                away_soup = get_soup(away_url)
-                away_info = get_value(soup=away_soup, selector=".wf-title")
+                away_soup = get_soup(url=away_url)
+                away_info = get_value(soup=away_soup, selector=".wf-title", attr="text", multiple=True)
                 away_name, away_alias = away_info[0], away_info[1]
 
-                bo_info = get_value(soup=soup, selector=".match-header-vs-note")[1]
-                score_info = get_value(soup=soup, selector=".js-spoiler", select_option=True)
-                home_score = score_info.split(":")[0].strip()
-                away_score = score_info.split(":")[-1].strip()
+                bo_info = get_value(soup=soup, selector=".match-header-vs-note", multiple=True)[1]
+                score_info = get_value(soup=soup, selector=".js-spoiler")
+                home_score = int(score_info.split(":")[0].strip())
+                away_score = int(score_info.split(":")[-1].strip())
 
-                h2h = get_value(soup=soup, selector=".match-h2h-matches-score")
+                h2h = get_value(soup=soup, selector=".match-h2h-matches-score", attr="text", multiple=True)
                 home_h2h_win, home_h2h_score = 0, 0
                 away_h2h_win, away_h2h_score = 0, 0
                 for value in h2h:
@@ -89,23 +94,25 @@ class MatchesScraper:
                     else:
                         away_h2h_win += 1
 
-                last_match = get_value(soup=soup, selector=".wf-card.mod-dark.match-histories", repeat=True)
-                home_last_match = get_value(soup=last_match[0], selector=".match-histories-item.wf-module-item", value="class")
-                away_last_match = get_value(soup=last_match[1], selector=".match-histories-item.wf-module-item", value="class")
-                home_last_match_win, away_last_match = 0, 0
+                last_match = get_value(soup=soup, selector=".wf-card.mod-dark.match-histories")
+                home_last_match = get_value(soup=last_match[0], selector=".match-histories-item.wf-module-item", attr="class")
+                away_last_match = get_value(soup=last_match[1], selector=".match-histories-item.wf-module-item", attr="class")
+                home_n_last_match_win, away_n_last_match_win = 0, 0
                 for i in [home_last_match, away_last_match]:
                     for value in i:
                         if "mod-win" not in value:
                             continue
                         elif i == home_last_match:
-                            home_last_match_win += 1
-                            home_last_match_wr = home_last_match_win / 5
+                            home_n_last_match_win += 1
+                            home_n_last_match = len(i)
                         elif i == away_last_match:
-                            away_last_match_win += 1
-                            away_last_match_wr = away_last_match_win / 5
+                            away_n_last_match_win += 1
+                            away_n_last_match = len(i)
+                        else:
+                            logging.info(f"{value} unindentified.")
 
-                map_selection = get_value(soup=soup, selector=".match-header-note", select_option=True)
-                if map_selection is None:
+                map_selection_container = get_value(soup=soup, selector=".match-header-note", attr="text")
+                if map_selection_container is None:
                     match = Match(
                         tour_id=tour_id,
                         match_id=match_id,
@@ -127,17 +134,24 @@ class MatchesScraper:
                         home_h2h_score=home_h2h_score,
                         away_h2h_score=away_h2h_score,
 
-                        home_last_5_wr=home_last_match_wr,
-                        away_last_5_wr=away_last_match_wr
+                        home_n_last_match=home_n_last_match,
+                        home_n_last_win=home_n_last_match_win,
+                        away_n_last_match=away_n_last_match,
+                        away_n_last_win=away_n_last_match_win
                     )
-                    results.append(match)
+
+                    matches_info.append(match)
+
+                    processed.add(url)
                     continue
 
+                # Need to seperate into different dataclass next update
+                map_selection = map_selection_container.split(";").strip()
                 home_map_picks, home_map_bans = [], []
                 away_map_picks, away_map_bans = [], []
                 decider_map = ""
                 for map in map_selection:
-                    map_split = map.strip().split(" ")
+                    map_split = map.split(" ").strip()
                     if map_split[0] == home_alias:
                         if map_split[1] == "pick":
                             home_map_picks.append(map_split[2].strip())
@@ -167,6 +181,16 @@ class MatchesScraper:
                     home_score=home_score,
                     away_score=away_score,
 
+                    home_h2h_win=home_h2h_win,
+                    away_h2h_win=away_h2h_win,
+                    home_h2h_score=home_h2h_score,
+                    away_h2h_score=away_h2h_score,
+
+                    home_n_last_match=home_n_last_match,
+                    home_n_last_win=home_n_last_match_win,
+                    away_n_last_match=away_n_last_match,
+                    away_n_last_win=away_n_last_match_win,
+
                     home_map_ban_1=home_map_bans[0],
                     home_map_ban_2=home_map_bans[1] if bo_info == "BO3" or bo_info == "BO1" else None,
                     home_map_ban_3=home_map_bans[2] if bo_info == "BO1" else None,
@@ -177,33 +201,29 @@ class MatchesScraper:
                     away_map_ban_3=away_map_bans[2] if bo_info == "BO1" else None,
                     away_map_pick_1=away_map_picks[0] if bo_info == "BO3" or bo_info == "BO5" else None,
                     away_map_pick_2=away_map_picks[1] if bo_info == "BO5" else None,
-                    decider_map=decider_map,
-
-                    home_h2h_win=home_h2h_win,
-                    away_h2h_win=away_h2h_win,
-                    home_h2h_score=home_h2h_score,
-                    away_h2h_score=away_h2h_score,
-
-                    home_last_5_wr=home_last_match_wr,
-                    away_last_5_wr=away_last_match_wr
+                    decider_map=decider_map
                 )
+
+                matches_info.append(match)
+
+                processed.add(url)
 
             end_time = datetime.now()
             duration = end_time - start_time
             logging.info(f"scrape_matches_info completed in {duration}s")
-            return results
+            return matches_info, tab_list
         
         except Exception as e:
-            logging.info(f"Error occurs when running scrape_matches_info: {e}")
+            logging.error(f"Error occurs when running scrape_matches_info: {e}")
             print(f"Error occurs when running scrape_matches_info: {e}")
     
-    def run(self, page: list):
+    def run(self, match_pages: list):
         start_time = datetime.now()
 
         logging.info("Initialize scrape_matches_list ...")
-        matches_list = self.scrape_matches_list(page=page)
+        matches_list = self.scrape_matches_list(match_page=match_pages)
         logging.info("Initialize scrape_matches_info ...")
-        matches_info = self.scrape_matches_info(page=matches_list)
+        matches_info, tab_list = self.scrape_matches_info(match_list=matches_list)
         matches_df = pd.DataFrame([asdict(m) for m in matches_info])
         path = r"E:\Valorant-Esports-Data-Pipeline-for-Analytics-and-Machine-Learning\data\raw\matches_raw.csv"
         matches_df.to_csv(path)
@@ -212,4 +232,7 @@ class MatchesScraper:
         end_time = datetime.now()
         duration = end_time - start_time
         logging.info(f"Matches scraper pipeline completed in {duration}s")
+        print("="*50)
         print(f"Matches scraper pipeline completed in {duration}s")
+        print("="*50)
+        return tab_list
