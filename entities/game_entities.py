8d60f9f987acf352f118d0a6ba8367a05ecc0540
unknown <khalish.21muhammad07@gmail.com>
2026-05-18 21:53:26 +0700

    Fixing Data Structure.

--- entities/game_entities.py
+++ entities/game_entities.py
@@ -1,34 +1,33 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
+from datetime import datetime
 
 @dataclass
 class Game:
     match_id: str
     game_id: str
-    map: str
-    duration: str
-    home_score: str
-    away_score: str
-    home_att_score: str
-    away_att_score: str
-    home_def_score: str
-    away_def_score: str
-    home_att_score: str
-    away_att_score: str
-    home_pstl_win: str
-    away_pstl_win: str
-    home_eco_round: str
-    away_eco_round: str
-    home_eco_win: str
-    away_eco_win: str
-    home_semi_eco_round: str
-    away_semi_eco_round: str
-    home_semi_eco_win: str
-    away_semi_eco_win: str
-    home_semi_buy_round: str
-    away_semi_buy_round: str
-    home_semi_buy_win: str
-    away_semi_buy_win: str
-    home_full_buy_round: str
-    away_full_buy_round: str
+    game_map: str
+    game_duration: datetime
+    home_score: int
+    away_score: int
+    home_att_score: int
+    away_att_score: int
+    home_def_score: int
+    away_def_score: int
+    home_pstl_win: int
+    away_pstl_win: int
+    home_eco_round: int
+    away_eco_round: int
+    home_eco_win: int
+    away_eco_win: int
+    home_semi_eco_round: int
+    away_semi_eco_round: int
+    home_semi_eco_win: int
+    away_semi_eco_win: int
+    home_semi_buy_round: int
+    away_semi_buy_round: int
+    home_semi_buy_win: int
+    away_semi_buy_win: int
+    home_full_buy_round: int
+    away_full_buy_round: int
     home_full_buy_win: int
     away_full_buy_win: int
