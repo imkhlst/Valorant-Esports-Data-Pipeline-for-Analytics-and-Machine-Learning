f350053f4281fc959cd65e7a511592ebe3898f12
unknown <khalish.21muhammad07@gmail.com>
2026-08-20 13:04:22 +0700

    Create staging tours SQL

---
+++ valorant/models/staging/stg_tours.sql
@@ -0,0 +1,8 @@
+SELECT
+    SAFE_CAST(tour_id AS STRING) AS tour_id,
+    SAFE_CAST(tour_name AS STRING) AS tour_name,
+    SAFE_CAST(tour_tag AS STRING) AS tour_tag,
+    SAFE_CAST(tour_stage AS STRING) AS tour_stage,
+    SAFE_CAST(tour_region AS STRING) AS tour_region,
+    SAFE_CAST(tour_status AS STRING) AS tour_status
+FROM {{ source('bronze', 'tours') }}
