3f9545ab5c50a5d301df1c282b7f6b19fca108a2
unknown <khalish.21muhammad07@gmail.com>
2026-08-30 18:05:37 +0700

    Update scrape pipeline

--- pipeline/scrape_pipeline.py
+++ pipeline/scrape_pipeline.py
@@ -1,4 +1,11 @@
-from src.tournaments_scraper import TournamentScraper
+from src.scraper.tournaments_scraper import TournamentScraper
+from src.scraper.matches_scraper import MatchesScraper
+from src.scraper.games_scraper import GamesScraper
 
+# file_path = r"E:\Valorant-Esports-Data-Pipeline-for-Analytics-and-Machine-Learning\data\link\tour.json"
 tournament_scraper = TournamentScraper()
 matches_page, stats_page, agents_page = tournament_scraper.run()
+match_scraper = MatchesScraper()
+matches = match_scraper.run(match_pages=matches_page)
+game_scraper = GamesScraper()
+games = game_scraper.run(tab_list=matches)
