93aff8bb7fd617711f6bfac98ead6fdb9fc3c13a
unknown <khalish.21muhammad07@gmail.com>
2026-09-10 11:41:00 +0700

    CI: Create scraper pipeline

--- pipeline/scraper_pipeline.py
+++ pipeline/scraper_pipeline.py
@@ -5,20 +5,23 @@ from src.scraper.matches_scraper import MatchesScraper
 from src.scraper.games_scraper import GamesScraper
 
 def main():
-    checkpoint = load_json(Path("data/checkpoint/pipeline_state.json"))
+        checkpoint = load_json(
+            Path("data/checkpoint/pipeline_state.json")
+            ) if Path("data/checkpoint/pipeline_state.json").exists() else {
+                "status": "ongoing", "module": "tournaments", "completed": False
+                }
 
-    if not checkpoint.completed and "tournaments" in checkpoint.module:
-        tournament_scraper = TournamentScraper()
-        tournament_scraper.run()
+        if not checkpoint.completed and "tournaments" in checkpoint.module:
+            tournament_scraper = TournamentScraper()
+            tournament_scraper.run()
+        
+        if not checkpoint.completed and "matches" in checkpoint.module:
+            match_scraper = MatchesScraper()
+            match_scraper.run(match_pages=Path("data/link/tours.json"))
 
-    if not checkpoint.completed and "matches" in checkpoint.module:
-        match_scraper = MatchesScraper()
-        match_scraper.run(match_pages=Path("data/link/tours.json"))
-
-    if not checkpoint.completed and "games" in checkpoint.module:
-        game_scraper = GamesScraper()
-        game_scraper.run(tab_list=Path("data/link/matches.json"))
-
+        if not checkpoint.completed and "games" in checkpoint.module:
+            game_scraper = GamesScraper()
+            game_scraper.run(tab_list=Path("data/link/matches.json"))
 
 if __name__ == "__main__":
     main()
